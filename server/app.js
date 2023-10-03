8452522356b2b50ed663cac4806a04ffd88067e1
Raviegupta <raviegupta153@gmail.com>
2023-10-03 09:38:01 +0530

    #4 middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+// Middleware functions are the functions that have access to the request object (req), the response object (res), 
+// and the next function in the application's request-response cycle.
+
+// The next function is a function in the Express router which, when invoked, executes the middleware succeeding the current middleware.
+
+const middleware = (req, res, next) => {
+  console.log(`Hello my Middleware`);
+  next();
+}
+// middleware();
+
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
-app.get('/about', (req, res) => {
+app.get('/about', middleware, (req, res) => {
   res.send('About Us Page');
 });
 app.get('/contact', (req, res) => {
